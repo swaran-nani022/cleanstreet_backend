fdeabfbf0b07bed1afb4e14e4364fdae9ce7ea78
Swaran Nani <swaran-nani022@gmail.com>
2026-05-28 12:10:05 +0530

    deploy

--- src/main/java/com/cleanstreet/backend/config/SecurityConfig.java
+++ src/main/java/com/cleanstreet/backend/config/SecurityConfig.java
@@ -4,22 +4,15 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-
 import org.springframework.security.config.http.SessionCreationPolicy;
-
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -80,8 +73,8 @@ public class SecurityConfig {
 
         configuration.setAllowedOrigins(
                 List.of(
-                        "http://localhost:5173",
-                        "http://localhost:5174",
+                        // "http://localhost:5173",
+                        // "http://localhost:5174",
                         "https://clean-street-sb-git-main-swarans-projects-8bcaad4b.vercel.app"
                 )
         );
